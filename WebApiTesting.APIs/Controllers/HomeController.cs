c99fb9f1978fe2a4989daf75a139390ca11c977b
Eakub Islam <eakubislam27@gmail.com>
2025-08-28 10:45:41 +0600

    feat: add setup for testing

---
+++ WebApiTesting.APIs/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiTesting.APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public string Index()
+        {
+            return "Hello World!";
+        }
+    }
+}
---
+++ WebApiTesting.UnitTests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiTesting.APIs.Controllers;
+
+namespace WebApiTesting.UnitTests.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Index_ReturnsHelloWorld()
+        {
+            // Arrange
+            var controller = new HomeController();
+            var expected = "Hello World!";
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
